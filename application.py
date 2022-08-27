72e3100389046966fc3d0090515169cda87ee823
ShubhamDalmia <shubham.dalmia@tercept.com>
2022-08-27 16:45:39 +0530

    it2

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
 from curses import flash
 from email.mime import application
 from email.mime.multipart import MIMEMultipart
-from flask import Flask,redirect,render_template,url_for,request
+from flask import Flask, redirect, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 import urllib.parse
 from sqlalchemy.orm import sessionmaker
+import deploy
 
 app = Flask(__name__)
 password = urllib.parse.quote_plus("CLi@1234")
@@ -15,36 +16,41 @@ session = Session()
 
 db = SQLAlchemy(app)
 
+
 @app.route('/')
 def index():
     return render_template('Login.html')
 
-@app.route('/Login',methods=['GET', 'POST'])
+
+@app.route('/Login', methods=['GET', 'POST'])
 def Login():
     if request.method == 'POST':
         userName = request.form['userName']
         passwordS = request.form['password']
-        isOwner = db.session.execute("select isOwner from DemoUser where userName='"+userName+"' and password='"+ passwordS+"'").all()
+        isOwner = db.session.execute(
+            "select isOwner from DemoUser where userName='" + userName + "' and password='" + passwordS + "'").all()
         if not isOwner:
             return ('Wrong Credentials')
         elif isOwner[0][0] == 1:
             return redirect(url_for('Owner'))
         elif isOwner[0][0] == 0:
             return redirect(url_for('Tenant'))
-        
 
-@app.route('/owner', methods=['POST','GET'])
+
+@app.route('/owner', methods=['POST', 'GET'])
 def Owner():
     cus = db.session.execute("select * from customers").all()
     properties = db.session.execute('select * from Property').all()
     print('oaky')
-    return render_template('owner.html',properties=properties)
+    return render_template('owner.html', properties=properties)
 
-@app.route('/tenant', methods=['POST','GET'])
+
+@app.route('/tenant', methods=['POST', 'GET'])
 def Tenant():
     return render_template('tenant.html')
 
-@app.route('/addTenant',methods=['POST'])
+
+@app.route('/addTenant', methods=['POST'])
 def AddTenant():
     firstName = request.form['firstName']
     lastName = request.form['lastName']
@@ -53,9 +59,13 @@ def AddTenant():
     phoneNo = request.form['phoneNo']
     linkedWallet = request.form['linkedWallet']
 
+    deploy.addTenant(linkedWallet, firstName, lastName, phoneNo, rating=0, rentalPoints=0, canRent=True, lastRentPaid=0,
+                     dueAmount=0)
+
     # query.format(name,userName,password,phoneNo,linkedWallet)
 
-@app.route('/addOwner',methods=['POST'])
+
+@app.route('/addOwner', methods=['POST'])
 def AddOwner():
     firstName = request.form['firstName']
     lastName = request.form['lastName']
@@ -64,8 +74,10 @@ def AddOwner():
     phoneNo = request.form['phoneNo']
     linkedWallet = request.form['linkedWallet']
     balance = 0
+
+    deploy.addOwner(linkedWallet, firstName, lastName, phoneNo, balance)
     query = "INSERT INTO DemoUser(Name,userName,password,PhoneNo,linkedWallet,isOwner) values('{0}','{1}','{2}','{3}','{4}',1);commit;"
-    query = query.format(firstName+' '+lastName,userName,password,phoneNo,linkedWallet,balance)
+    query = query.format(firstName + ' ' + lastName, userName, password, phoneNo, linkedWallet, balance)
     test = db.session.execute(query)
     # uid = db.session.execute("select max(Uid) from DemoUser where userName='"+userName+"'").all()
     # propInsertQuery = "Insert into Property(Uid,Address,ApplicationNo) values('{0}','-','{1}');commit;"
@@ -74,19 +86,21 @@ def AddOwner():
     # test = db.session.execute
     return redirect(url_for('Owner'))
 
-@app.route('/addProperty', methods = ['POST'])
+
+@app.route('/addProperty', methods=['POST'])
 def AddProperty():
     propertyId = request.form['propertyId']
     linkedWallet = request.form['linkedWallet']
     isCurrentlyRented = request.form['isCurrentlyRented']
     if isCurrentlyRented == 'on':
         isCurrentlyRented = 1
-    else :
+    else:
         isCurrentlyRented = 0
     rent = request.form['rent']
 
+    deploy.addProperty(linkedWallet, propertyId, isCurrentlyRented, rent)
     propInsertQuery = "Insert into Property(propertyId,linkedWallet,isCurrentlyRented,rent) values('{0}','{1}','{2}','{3}');commit;"
-    propInsertQuery = propInsertQuery.format(propertyId, linkedWallet,isCurrentlyRented,rent)
+    propInsertQuery = propInsertQuery.format(propertyId, linkedWallet, isCurrentlyRented, rent)
     test = db.session.execute(propInsertQuery)
     return redirect(url_for('Owner'))
 
@@ -94,6 +108,7 @@ def AddProperty():
 @app.route('/logout')
 def Logout():
     return render_template('Login.html')
+
 
 if __name__ == "__main__":
     app.run(debug=False)
